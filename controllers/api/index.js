f044eb8a2a67d648c069d912095035cfe52e4ffa
Julia Eason <jmeason00@gmail.com>
2024-07-15 12:28:32 -0400

    adding router.use

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('');
 const eventRoutes = require('');
+
+router.use('', userRoutes);
+router.use('', eventRoutes);
+
+module.exports=router
