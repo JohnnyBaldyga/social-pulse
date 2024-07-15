d5866fd373847e7a33277a99b6615b2fe1763bbc
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-07-15 10:43:42 -0400

    get id route

--- controllers/api/eventroutes.js
+++ controllers/api/eventroutes.js
@@ -10,12 +10,12 @@ router.get("/", async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ["name"],
+          attributes: ["name", "description", "event_date", "location"],
         },
       ],
     });
     const events = eventData.map((event) => event.get({ plain: true }));
-    res.render("homepage", {
+    res.render("event", {
       events,
       logged_in: req.session.logged_in,
     });
@@ -25,6 +25,31 @@ router.get("/", async (req, res) => {
 });
 
 // GET event by id
+router.get("/event/:id", async (req, res) => {
+  try {
+    const eventData = await Event.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: [
+            "name",
+            "description",
+            "event_date",
+            "date_created",
+            "location",
+          ],
+        },
+      ],
+    });
+    const event = eventData.get({ plain: true });
+    res.render("event", {
+      ...event,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // POST event
 
