ea7009111b3f3c388cb64c44fa0bd0a1f8510437
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-07-16 12:31:45 -0400

    signup route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,37 +1,52 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// POST Sign Up
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    res.status(201).json({ message: "User created!", user: newUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // POST login
 
-router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-// Check if password and email are valid
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
 
-const validPassword = await userData.checkPassword(req.body.password);
+    // Check if password and email are valid
+
+    const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
 
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
   } catch (err) {
     res.status(400).json(err);
   }
@@ -39,14 +54,14 @@ const validPassword = await userData.checkPassword(req.body.password);
 
 // POST logout
 
-router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
