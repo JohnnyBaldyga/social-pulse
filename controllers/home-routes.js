9a2127292334fe48a8e4c91b7cb18fa88c8b714a
Adam Mauch <maucap20@wfu.edu>
2024-07-17 11:39:44 -0400

    fix login on handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -21,10 +21,10 @@ router.get("/", async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/login');
-      return;
-    }
+    // if (req.session.logged_in) {
+    //   res.redirect('/');
+    //   return;
+    // }
   
     res.render('login-view');
   });
