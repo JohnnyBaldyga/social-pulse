44290e8367c767780c8822602816bab87d6d9a61
Julia Eason <jmeason00@gmail.com>
2024-07-17 13:19:01 -0400

    adding delte button handle

--- public/js/event.js
+++ public/js/event.js
@@ -16,11 +16,19 @@ const newFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/event');
+      document.location.replace('api/event');
     } else {
       alert('Failed to search event');
     }
   }
 };
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/event/${id}`, {
+      method: 'DELETE',
+    });
+
 
 
